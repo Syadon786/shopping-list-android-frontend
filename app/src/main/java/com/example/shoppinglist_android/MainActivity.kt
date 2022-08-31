1b28091d733bc272b3b3bafa765a370951d81f8c
Syadon <bokagabor4@gmail.com>
2022-09-01 01:15:34 +0200

    refresh button added

--- app/src/main/java/com/example/shoppinglist_android/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist_android/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.shoppinglist_android
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsAnimation
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,16 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val url = "https://syadon-shoppinglist.herokuapp.com/"
 
         // WebViewClient allows you to handle
         // onPageFinished and override Url loading.
         webView.webViewClient = WebViewClient()
 
         // this will load the url of the website
-        webView.loadUrl("https://syadon-shoppinglist.herokuapp.com/")
+        webView.loadUrl(url)
 
         // this will enable the javascript settings
         webView.settings.javaScriptEnabled = true
+
+        btn_reload.setOnClickListener {
+            this.webView.loadUrl(url)
+        }
+
+
     }
 
     // if you press Back button this code will work
